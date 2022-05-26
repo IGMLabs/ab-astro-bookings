d225afe6421afa7ba6d7475bd9a93e1b45897b31
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-26 11:33:38 +0200

    feat: list of agencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ab-astro-bookings';
+  public title = 'Astro Bookings';
+  public subtitle = 'Welcome on board';
+  public author = 'Alberto Basalo';
+  public authorUrl = 'https://twitter.com/albertobasalo';
+  public agencies = [
+    {
+      id: 'space-y',
+      name: 'Space Y',
+      range: 'Interplanetary',
+      status: 'Active',
+    },
+    {
+      id: 'green-origin',
+      name: 'Green Origin',
+      range: 'Orbital',
+      status: 'Active',
+    },
+    {
+      id: 'virgin-way',
+      name: 'Virgin Way',
+      range: 'Orbital',
+      status: 'Pending',
+    },
+  ];
+  public reloading = false;
+  public getAgenciesLength() {
+    return this.agencies.length;
+  }
+  public reload(list: string) {
+    this.reloading = true;
+    console.log('Reloading...' + list);
+  }
 }
