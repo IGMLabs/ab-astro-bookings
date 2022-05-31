4a64815e19e1aefce7dd0e6b9b9585e0599b2eaf
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-31 12:31:05 +0200

    feat: no selector for contact page

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contact',
   templateUrl: './contact.page.html',
-  styleUrls: ['./contact.page.css']
+  styleUrls: ['./contact.page.css'],
 })
 export class ContactPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
