7cf79a80fe5c0fdd53c84342396cc5601a93da73
Alberto Basalo <albertobasalo@hotmail.com>
2022-06-02 11:38:03 +0200

    feat: get id from activated route

--- src/app/agencies/agency/agency.page.ts
+++ src/app/agencies/agency/agency.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-agency',
   templateUrl: './agency.page.html',
-  styleUrls: ['./agency.page.css']
+  styleUrls: ['./agency.page.css'],
 })
 export class AgencyPage implements OnInit {
+  public agencyId: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(route: ActivatedRoute) {
+    this.agencyId = route.snapshot.paramMap.get('id') || '';
   }
 
+  ngOnInit(): void {}
 }
