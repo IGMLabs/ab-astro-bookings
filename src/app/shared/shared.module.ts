b4547797e77a3c0ee5c35cc8126daa746e318b3d
Alberto Basalo <albertobasalo@hotmail.com>
2022-06-02 11:25:41 +0200

    feat: rout link

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AgenciesList } from './components/agencies/agencies.list';
 import { ReloadingComponent } from './components/reloading/reloading.component';
 import { TripsList } from './components/trips/trips.list';
 
 @NgModule({
   declarations: [ReloadingComponent, AgenciesList, TripsList],
-  imports: [CommonModule],
-  exports: [ReloadingComponent, AgenciesList, TripsList],
+  imports: [CommonModule, RouterModule],
+  exports: [RouterModule, ReloadingComponent, AgenciesList, TripsList],
 })
 export class SharedModule {}
