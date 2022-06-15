ec786c8565ee1850ae8fe06b80a050dddd6e76c9
Alberto Basalo <albertobasalo@hotmail.com>
2022-06-15 13:07:09 +0200

    feat: search by query params

--- src/app/agencies/agencies.page.ts
+++ src/app/agencies/agencies.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { BehaviorSubject, Observable, switchMap } from 'rxjs';
+import { BehaviorSubject, map, Observable, switchMap } from 'rxjs';
 import { AgenciesApi } from '../core/api/agencies.api';
 import { Agency } from '../core/api/agency.interface';
 import { Trip } from '../core/api/trip.interface';
+import { TripsApi } from '../core/api/trips.api';
 
 @Component({
   selector: 'app-agencies',
@@ -15,18 +16,20 @@ export class AgenciesPage implements OnInit {
   public trips$!: Observable<Trip[]>;
   private search$: BehaviorSubject<string> = new BehaviorSubject('');
 
-  constructor(private agenciesApi: AgenciesApi, private route: ActivatedRoute) {
+  constructor(
+    private agenciesApi: AgenciesApi,
+    private tripsApi: TripsApi,
+    private route: ActivatedRoute
+  ) {
     this.agencies$ = this.search$.pipe(
       // map((searchTerm) => this.agenciesApi.getByText$(searchTerm))
       switchMap((searchTerm) => this.agenciesApi.getByText$(searchTerm))
       // concatMap((searchTerm) => this.agenciesApi.getByText$(searchTerm))
       // exhaustMap((searchTerm) => this.agenciesApi.getByText$(searchTerm))
     );
-
-    // const q = this.route.snapshot.queryParamMap.get('q');
-    // console.log(q);
-    this.route.queryParamMap.subscribe((queryParamMap) =>
-      console.log(queryParamMap.get('q'))
+    this.trips$ = this.route.queryParamMap.pipe(
+      map((queryParamMap) => queryParamMap.get('q')),
+      switchMap((agencyId) => this.tripsApi.getByText$(agencyId))
     );
   }
 
